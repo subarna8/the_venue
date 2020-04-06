39703d267d9fe25f3b77731e7705c36de8895029
subarna8 <mahmuda.akhter019@gmail.com>
2020-04-07 03:02:01 +0600

    completedwithalittlebug

--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -1,37 +1,51 @@
 import React from 'react';
+import { scroller } from 'react-scroll';
 
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
 const SideDrawer = (props) => {
+
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element,{
+            duration: 1500,
+            delay:100,
+            smooth: true,
+            offset: -150
+        });
+        props.onClose(false)
+    }
+
     return (
-        <div>
-            <Drawer
-                anchor="right"
-                open={props.open}
-                onClose={()=> props.onClose(false)}
-            >
-                
-                <List component="nav">
-                    <ListItem button onClick = {()=> console.log('Featured')}>
-                        Event starts In
-                    </ListItem>
-                    <ListItem button onClick = {()=> console.log('Vanue info')}>
-                       Venue Info
-                    </ListItem>
-                    <ListItem button onClick = {()=> console.log('HighLights')}>
-                        HighLights
-                    </ListItem>
-                    <ListItem button onClick = {()=> console.log('Pricing')}>
-                        Pricing
-                    </ListItem>   
-                    <ListItem button onClick = {()=> console.log('Location')}>
-                        Location
-                    </ListItem> 
-                </List>
-            </Drawer>
-        </div>
+        <Drawer
+            anchor="right"
+            open={props.open}
+            onClose={()=> props.onClose(false)}
+        >
+            <List component="nav">
+                <ListItem button onClick={()=> scrollToElement('featured')}>
+                    Event starts in
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('venuenfo')}>
+                    Venue NFO
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('highlights')}>
+                    Highlights
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('pricing')}>
+                    Pricing
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('location')}>
+                    Location
+                </ListItem>
+
+            </List> 
+        </Drawer>
     );
 };
 
